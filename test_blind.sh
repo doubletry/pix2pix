db7b0b82d1521a7308fbd75dd9d23f0c2f59e8bd
doubletry <654507985@qq.com>
2018-10-16 20:43:57 +0800

    run this to test the model

--- test_blind.sh
+++
@@ -1,1 +0,0 @@
-DATA_ROOT=./datasets/all_color/data_for_train/ name=blind_generati_all which_direction=AtoB phase=test th test.lua
---
+++ test_model.sh
@@ -0,0 +1,1 @@
+DATA_ROOT=./datasets/colourblindness/ name=colourblindness which_direction=AtoB phase=test th test.lua
