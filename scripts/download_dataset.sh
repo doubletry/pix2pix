f737a2e38f4ead20e7f41f77a798dfa527a357e1
doubletry <654507985@qq.com>
2018-10-17 12:35:01 +0800

    run this to download the dataset

--- scripts/download_dataset.sh
+++ scripts/download_dataset.sh
@@ -1,2 +1,2 @@
-wget -c --referer=https://pan.baidu.com/s/1rCJt8yVkkfunlvU54Qxn_g -O dataset.zip -P ./datasets/ "https://www.baidupcs.com/rest/2.0/pcs/file?method=batchdownload&app_id=250528&zipcontent=%7B%22fs_id%22%3A%5B407733683563270%5D%7D&sign=DCb740ccc5511e5e8fedcff06b081203:tpXqf%2FU7kcxf%2FT2OiVSOP1eQSxk%3D&uid=522538700&time=1539832381&dp-logid=6713565912049522764&dp-callid=0&vuk=522538700&zipname=dataset.zip"
-unzip ./datasets/dataset.zip
+wget -c --referer=https://pan.baidu.com/s/1rCJt8yVkkfunlvU54Qxn_g -O ./datasets/dataset.zip "https://www.baidupcs.com/rest/2.0/pcs/file?method=batchdownload&app_id=250528&zipcontent=%7B%22fs_id%22%3A%5B407733683563270%5D%7D&sign=DCb740ccc5511e5e8fedcff06b081203:tpXqf%2FU7kcxf%2FT2OiVSOP1eQSxk%3D&uid=522538700&time=1539832381&dp-logid=6713565912049522764&dp-callid=0&vuk=522538700&zipname=dataset.zip"
+unzip ./datasets/dataset.zip -d ./datasets/
